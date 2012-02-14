4e383a4994b69197ed224056299b25fd5cf946bc
Dana Klassen <klassen.dana@gmail.com>
2012-02-13 20:42:53 -0500

    added generate_mapping.sh script to extract seeAlso annotations and build mapping file

---
+++ scripts/generate_mapping.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+########################################################
+# Description ::  Script to generate mapping of mechtox 
+# ontology to other ontology. This script relies on the 
+# fact that relations between classes in external
+# ontologies were made using the 'seeAlso' annotation
+# created in the Protege OWL ontology editor software.
+# Author :: Dana Klassen
+########################################################
+
+# Covert the ontology to 
